aa00da092bb917d2cbd80c826d70f5fe00ca36be
ar3asault <47409546+ar3asault@users.noreply.github.com>
2020-06-16 00:43:19 -0400

    updated extruder motor

--- firmware/V2.0/Marlin-2.0.x-SKR-mini-E3-V2.0/Marlin/Configuration.h
+++ firmware/V2.0/Marlin-2.0.x-SKR-mini-E3-V2.0/Marlin/Configuration.h
@@ -723,7 +723,7 @@
  * Override with M92
  *                                      X, Y, Z, E0 [, E1[, E2...]]
  */
-#define DEFAULT_AXIS_STEPS_PER_UNIT   { 160, 160, 400, 838 }
+#define DEFAULT_AXIS_STEPS_PER_UNIT   { 160, 160, 400, 415 }
 
 /**
  * Default Max Feed Rate (mm/s)
